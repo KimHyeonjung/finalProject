7a1382418e770589604ba3acf03c5d887dc6d987
skystar00414 <127706149+skystar00414@users.noreply.github.com>
2024-10-01 16:15:46 +0900

    잘가 내 행복했던 코드들아

--- market/src/main/java/com/team3/market/dao/MemberDAO.java
+++
@@ -1,9 +0,0 @@
-package com.team3.market.dao;
-
-import com.team3.market.model.vo.MemberVO;
-
-public interface MemberDAO {
-
-	boolean insertMember(MemberVO me);
-
-}
--- market/src/main/java/com/team3/market/model/vo/WayVO.java
+++
@@ -1,9 +0,0 @@
-package com.team3.market.model.vo;
-
-import lombok.Data;
-
-@Data
-public class WayVO {
-	private int way_num; 
-	private String way_name;
-}
