f99cf88210d320603b020f899be0cb1fbad75bba
skystar00414 <127706149+skystar00414@users.noreply.github.com>
2024-10-01 15:16:27 +0900

    기능 작동 시 메시지 출력

--- market/src/main/java/com/team3/market/controller/Login.java
+++ market/src/main/java/com/team3/market/controller/Login.java
@@ -4,13 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 @Controller
 @NoArgsConstructor
 public class Login {
+	
 	@GetMapping("/login")
-	public String login() {
+	public String Login() {
 		return "/login";
 	}
+	
+	@PostMapping("/login")
+	public String Login(@RequestParam("member_id")String me_id, @RequestParam("member_pw")String me_pw) {
+		System.out.printf(me_id);
+		System.out.printf(me_pw);
+		return "/index";
+	}
+	
+	
 }
--- market/src/main/java/com/team3/market/controller/SignUp.java
+++ market/src/main/java/com/team3/market/controller/SignUp.java
@@ -1,6 +1,7 @@
 package com.team3.market.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,11 +24,17 @@ public class SignUp {
 	}
 	
 	@PostMapping("/signup")
-	public String SignUp(MemberVO member) {
+	public String SignUp(Model model, MemberVO member) {
+		if (member == null || member.getMember_id() == "" || member.getMember_pw() == "" || member.getMember_nick() == "" || member.getMember_phone() == "" || member.getMember_email() == "") {
+			model.addAttribute("message", "회원가입에 실패하였습니다.");
+			return "/signup";
+		}
 		boolean res = memberService.insertMember(member);
 		if (res) {
+			model.addAttribute("message", "회원가입이 성공적으로 완료되었습니다!");
 			return "/index";
 		}
+		model.addAttribute("message", "회원가입에 실패하였습니다.");
 		return "/signup";
 	}
 	
