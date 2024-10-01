98ca881da7f7e2e8de66a757b883c304e8e01d6c
skystar00414 <127706149+skystar00414@users.noreply.github.com>
2024-10-01 12:29:17 +0900

    회원가입 기능 구현

--- market/src/main/java/com/team3/market/controller/SignUp.java
+++ market/src/main/java/com/team3/market/controller/SignUp.java
@@ -4,13 +4,31 @@ import org.springframework.stereotype.Controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.team3.market.model.vo.MemberVO;
+import com.team3.market.service.MemberService;
+
 
 
 @Controller
 @AllArgsConstructor
 public class SignUp {
+	
+	private MemberService memberService;
+	
 	@GetMapping("/signup")
 	public String SignUp() {
 		return "/signup";
 	}
+	
+	@PostMapping("/signup")
+	public String SignUp(MemberVO member) {
+		boolean res = memberService.insertMember(member);
+		if (res) {
+			return "/index";
+		}
+		return "/signup";
+	}
+	
 }
