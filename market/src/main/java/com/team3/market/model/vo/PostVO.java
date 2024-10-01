676d13f9802fe0526d01556a7af6e115265b2679
skystar00414 <127706149+skystar00414@users.noreply.github.com>
2024-10-01 10:58:03 +0900

    PostVO생성

---
+++ market/src/main/java/com/team3/market/model/vo/PostVO.java
@@ -0,0 +1,23 @@
+package com.team3.market.model.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PostVO {
+	private int  post_num;
+	private int post_member_num;
+	private int post_position_num;
+	private int post_way_num;
+	private int post_category_num;
+	private String post_title;
+	private String post_content;
+	private int post_price;
+	private boolean post_deal;
+	private Date post_date;
+	private Date post_refresh;
+	private String post_address;
+}
