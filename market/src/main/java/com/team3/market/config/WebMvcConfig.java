ce02d0bdca172472e95774af7171b911bd8cf3a4
skystar00414 <127706149+skystar00414@users.noreply.github.com>
2024-10-08 11:48:39 +0900

    Update WebMvcConfig.java

--- market/src/main/java/com/team3/market/config/WebMvcConfig.java
+++ market/src/main/java/com/team3/market/config/WebMvcConfig.java
@@ -5,10 +5,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+
+import com.team3.market.interceptor.MemberInterceptor;
 
 @Configuration
 @EnableWebMvc
