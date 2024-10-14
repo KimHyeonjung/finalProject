e9a439ce1d381f12552b2905f6b1e017f773ad06
skystar00414 <127706149+skystar00414@users.noreply.github.com>
2024-10-14 16:57:28 +0900

    채팅방 리스트 출력

--- market/src/main/java/com/team3/market/service/ChatService.java
+++ market/src/main/java/com/team3/market/service/ChatService.java
@@ -1,22 +1,46 @@
 package com.team3.market.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.team3.market.dao.ChatDAO;
+import com.team3.market.model.dto.ChatRoomDTO;
 import com.team3.market.model.vo.ChatRoomVO;
+import com.team3.market.model.vo.ChatVO;
+import com.team3.market.model.vo.MemberVO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ChatService {
 
-	@Autowired
-    private ChatDAO chatDAO; // ChatDAO를 필드 주입
+    @Autowired
+    private ChatDAO chatDAO; // ChatDAO 주입
 
-    // 모든 채팅방 목록을 가져오는 메서드
-    public List<ChatRoomVO> getAllChatRooms() {
-        return chatDAO.getAllChatRooms(); // ChatDAO의 메서드 호출
+	public List<ChatRoomVO> getChatRoomsByMember(int member_num) {
+		return chatDAO.selectChatRoomsByMember(member_num);
+	}
+	
+	public List<ChatRoomDTO> getChatRoomsWithMembers(int member_num) {
+        List<ChatRoomVO> chatRooms = chatDAO.selectChatRoomsByMember(member_num);
+        List<ChatRoomDTO> chatRoomWithMembers = new ArrayList<ChatRoomDTO>();
+        
+
+        for (ChatRoomVO chatRoom : chatRooms) {
+            // 발신자 정보를 DB에서 가져옴
+            MemberVO targetMember = getSenderByChatRoomId(chatRoom.getChatRoom_num());
+            // 최신 채팅 메시지 가져오기
+            ChatVO lastChat = chatDAO.selectLatestChatByRoom(chatRoom.getChatRoom_num());
+            chatRoomWithMembers.add(new ChatRoomDTO(chatRoom, targetMember, lastChat));
+        }
+        
+        return chatRoomWithMembers;
     }
 
+	private MemberVO getSenderByChatRoomId(int chatRoom_num) {
+		return chatDAO.selectSenderByChatRoom(chatRoom_num);
+	}
+
+    
 }
