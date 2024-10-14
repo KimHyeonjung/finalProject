67669e27b38e73fddee32379c9cec849856cb392
skystar00414 <127706149+skystar00414@users.noreply.github.com>
2024-10-14 14:25:04 +0900

    chatdao생성

--- market/src/main/java/com/team3/market/dao/ChatDAO.java
+++ market/src/main/java/com/team3/market/dao/ChatDAO.java
@@ -2,10 +2,19 @@ package com.team3.market.dao;
 
 import java.util.List;
 
-import com.team3.market.model.vo.ChatRoomVO;
+import org.apache.ibatis.annotations.Mapper;
 
+import com.team3.market.model.vo.ChatRoomVO;
+import com.team3.market.model.vo.ChatVO;
+import com.team3.market.model.vo.MemberVO;
+
+@Mapper
 public interface ChatDAO {
 
-	List<ChatRoomVO> getAllChatRooms();
+	List<ChatRoomVO> selectChatRoomsByMember(int member_num);
 
+	MemberVO selectSenderByChatRoom(int chatRoom_num);
+
+	ChatVO selectLatestChatByRoom(int chatRoom_num);
+    
 }
