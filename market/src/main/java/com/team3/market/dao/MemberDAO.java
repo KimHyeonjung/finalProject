712b206e93ca7b128288c1e2c52e84ca1b6a3d7e
hwh019 <hwh019@gmail.com>
2024-10-15 16:25:13 +0900

    임시 커밋

--- market/src/main/java/com/team3/market/dao/MemberDAO.java
+++ market/src/main/java/com/team3/market/dao/MemberDAO.java
@@ -1,6 +1,7 @@
 package com.team3.market.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.team3.market.model.vo.MemberVO;
 
@@ -28,5 +29,12 @@ public interface MemberDAO {
 	boolean updateemail(MemberVO user);
 
 	boolean updatephone(MemberVO user);
+
+	MemberVO findMemberId(@Param("member_nick")String member_nick,@Param("member_email") String member_email);
+
+	MemberVO findMemberPw(@Param("member_id") String memberId, 
+			 @Param("member_nick") String memberNick, 
+             @Param("member_email") String memberEmail);
+	
 
 }
