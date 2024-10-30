06f1dc8139d27bf8611b31e125b6f75162097bf5
skystar00414 <127706149+skystar00414@users.noreply.github.com>
2024-10-30 10:46:05 +0900

    임시

--- market/src/main/java/com/team3/market/model/vo/NotificationVO.java
+++ market/src/main/java/com/team3/market/model/vo/NotificationVO.java
@@ -12,6 +12,7 @@ public class NotificationVO {
 	private int notification_member_num;
 	private int notification_type_num;
 	private int notification_post_num;
+	private int notification_chatRoom_num;
 	private String notification_message; 
 	private boolean notification_read; 
 	private Date notification_date;
